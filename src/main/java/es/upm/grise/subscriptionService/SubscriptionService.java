e30eb0fbfd50de59fc5c0e2698b86a5ea240ff51
odieste <odieste@fi.upm.es>
2026-07-14 09:23:39 +0200

    Initial commit

---
+++ src/main/java/es/upm/grise/subscriptionService/SubscriptionService.java
@@ -0,0 +1,39 @@
+package es.upm.grise.subscriptionService;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class SubscriptionService {
+
+	private Collection <User> subscribers;
+	private EmailService emailService;
+	
+	/* 
+	 * Constructor
+	 */
+	
+	public SubscriptionService(EmailService emailService) {}
+
+	/* 
+	 * Method to code/test
+	 */
+	
+	public void addSubscriber(User user) {}
+	
+	/* 
+	 * Method to code/test
+	 */
+	
+	public void removeSubscriber(User user) {}
+	
+	/* 
+	 * Method to code/test
+	 */
+	
+	public int sendMessage(Message message) {
+		
+		return 0;
+		
+	}
+
+}
