eb849b66f68be426f87238cb7411d7edcdbfa415
Danny Tiupul <dannytiupul@gmail.com>
2026-07-16 08:33:12 -0500

    Codigo implementado

--- src/main/java/es/upm/grise/subscriptionService/SubscriptionService.java
+++ src/main/java/es/upm/grise/subscriptionService/SubscriptionService.java
@@ -8,32 +8,68 @@ public class SubscriptionService {
 	private Collection <User> subscribers;
 	private EmailService emailService;
 	
-	/* 
-	 * Constructor
-	 */
-	
-	public SubscriptionService(EmailService emailService) {}
+	public SubscriptionService(EmailService emailService) {
+        this.emailService = emailService;
+        this.subscribers = new ArrayList<>();
+    }
 
-	/* 
-	 * Method to code/test
-	 */
-	
-	public void addSubscriber(User user) {}
-	
-	/* 
-	 * Method to code/test
-	 */
-	
-	public void removeSubscriber(User user) {}
-	
-	/* 
-	 * Method to code/test
-	 */
-	
-	public int sendMessage(Message message) {
-		
-		return 0;
-		
-	}
+    public void addSubscriber(User user)
+            throws NullUserException, ExistingUserException, UserDoesNotHaveEmailException {
 
+        if (user == null) {
+            throw new NullUserException();
+        }
+
+        if (subscribers.contains(user)) {
+            throw new ExistingUserException();
+        }
+
+        if (user.getDeliveryType() != Delivery.LOCAL && user.getEmail() == null) {
+            throw new UserDoesNotHaveEmailException();
+        }
+
+        subscribers.add(user);
+    }
+
+    public void removeSubscriber(User user)
+            throws NullUserException, NonExistingUserException {
+
+        if (user == null) {
+            throw new NullUserException();
+        }
+
+        if (!subscribers.contains(user)) {
+            throw new NonExistingUserException();
+        }
+
+        subscribers.remove(user);
+    }
+
+    public int sendMessage(Message message) {
+
+        int discarded = 0;
+
+        for (User user : subscribers) {
+
+            switch (user.getDeliveryType()) {
+
+                case EMAIL:
+                    emailService.sendMessage(user, message);
+                    break;
+
+                case LOCAL:
+                    user.saveMessage(message);
+                    break;
+
+                case DO_NOT_DELIVER:
+                    user.saveMessage(
+                        new Message(message.getId(), "Ha perdido ud. un mensaje"));
+                    discarded++;
+                    break;
+            }
+        }
+
+        return discarded;
+    }
+	
 }
